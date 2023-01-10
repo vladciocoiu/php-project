3b4adb2040b41242333fb0043d081b821d38259f
Vlad Ciocoiu <vlad.014916@gmail.com>
2023-01-11 00:42:04 +0200

    small changes

--- project/db_connect.php
+++ project/db_connect.php
@@ -1,8 +1,10 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "test";
+    require_once __DIR__ . "/../load_env.php";
+
+    $servername = $_ENV['DB_SERVER_NAME'];
+    $username = $_ENV['DB_USERNAME'];
+    $password = $_ENV['DB_PASSWORD'];
+    $dbname = $_ENV['DB_NAME'];
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
